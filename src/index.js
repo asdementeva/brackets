53709501e5ed94bbd6754d4731e583ad1566c7e3
Alexandra Dementeva <102434370+asdementeva@users.noreply.github.com>
2023-10-27 14:19:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   const stack = [];
+  const openBrackets = new Set();
+  const bracketMap = new Map();
+
+  for (const [open, close] of bracketsConfig) {
+    openBrackets.add(open);
+    bracketMap.set(close, open);
+  }
+
+  for (const char of str) {
+    if (openBrackets.has(char)) {
+      stack.push(char);
+    } else {
+      if (stack.length === 0) {
+        return false; // Unmatched closing bracket
+      }
+
+      const top = stack.pop();
+      if (top !== bracketMap.get(char)) {
+        return false; // Mismatched brackets
+      }
+    }
+  }
+
+  return stack.length === 0; // Stack should be empty for a valid sequence
+
 }
